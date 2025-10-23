192e814ba467e7d8fa056a998392f23e4674bffc
ElSheria <sheriaelie@gmail>
2025-10-23 14:53:45 +0200

    feat: add greet function with default name management.

--- 02-saluer.js
+++ 02-saluer.js
@@ -12,7 +12,12 @@
 function saluer(nom) {
   // Exercice non implémenté : afficher Bonjour, [nom] ou Bonjour, inconnu
   // Placeholder neutre : ne fait rien et retourne undefined.
+if (nom) {
+    console.log(`Bonjour, ${nom}`);
+  } else {
+    console.log("Bonjour, inconnu");
+  }
 }
-
+saluer("helma");
 // Ne pas modifier la ligne ci-dessous
 module.exports = { saluer }
